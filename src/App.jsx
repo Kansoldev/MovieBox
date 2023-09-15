a099e6162f14e8c35739fa349451d4c7e8985abe
oyinkansola <yahayaoyinkansola@gmail.com>
2023-09-15 18:42:39 +0100

    Update import for Movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Movies from "./components/Movies";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Movies />
     </>
   );
 }
