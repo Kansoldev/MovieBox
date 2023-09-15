d2942962d4a9b4168aeb1c178ba00b137a930c74
oyinkansola <yahayaoyinkansola@gmail.com>
2023-09-15 09:56:17 +0100

    Import Hero and Header to display UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 function App() {
   return (
     <>
-      <h1>New project</h1>
+      <Header />
+      <Hero />
     </>
   );
 }
