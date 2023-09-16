6ec060faec261bfe1b763e5de08760391ac1b0c2
oyinkansola <yahayaoyinkansola@gmail.com>
2023-09-17 00:12:56 +0100

    Move components to a new file To help handle redirection to pages easier

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Hero from "./Hero";
+import Movies from "./Movies";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Movies />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
